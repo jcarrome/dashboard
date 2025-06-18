a7345b2533b8beaec4f8e676e87e89588b537e12
Juan Carlos Romero Uchubanda <152119104+jcarrome@users.noreply.github.com>
2025-06-18 19:48:36 +0000

    Correcto

--- src/App.tsx
+++ src/App.tsx
@@ -11,43 +11,25 @@ function App() {
       <Grid container spacing={5} justifyContent="center" alignItems="center">
 
          {/* Encabezado */}
-         <Grid item xs={12} md={12}>Elemento: Encabezado</Grid>
+         <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
 
          {/* Alertas */}
-         <Grid item xs={12} md={12}>Elemento: Alertas</Grid>
+         <Grid size={{ xs: 12, md: 12 }}>Elemento: Alertas</Grid>
 
          {/* Selector */}
-         <Grid item xs={12} md={3}>Elemento: Selector</Grid>
+         <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
 
          {/* Indicadores */}
-         <Grid item xs={12} md={9}>Elemento: Indicadores</Grid>
+         <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
 
          {/* Gráfico */}
-         <Grid
-  item
-  xs={12}
-  md={6}
-  sx={{
-    display: { xs: 'none', md: 'block' }
-  }}
->
-  Elemento: Gráfico
-</Grid>
+         <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: 'none', md: 'block' } }}>Elemento: Gráfico</Grid>
 
-{/* Tabla */}
-<Grid
-  item
-  xs={12}
-  md={6}
-  sx={{
-    display: { xs: 'none', md: 'block' }
-  }}
->
-  Elemento: Tabla
-</Grid>
+         {/* Tabla */}
+         <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: 'none', md: 'block' } }}>Elemento: Tabla</Grid>
 
          {/* Información adicional */}
-         <Grid item xs={12} md={12}>Elemento: Información adicional</Grid>
+         <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
 
       </Grid>
    );
